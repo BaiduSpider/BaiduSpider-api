ea4963a859cff73caf5e7deff60a398d5849b55f
Sam Zhang <samzhang951@outlook.com>
2021-02-13 18:30:03 +0800

    Change API framework to Flask

--- app.py
+++ app.py
@@ -4,93 +4,104 @@ TODO: 添加关于API的文档
 """
 import os
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, request
+from flask_cors import CORS
 
 import os
 import sys
 
 from baiduspider import BaiduSpider
 
-app = FastAPI()
+app = Flask(__name__)
+cors = CORS(app, supports_credentials=True)
 spider = BaiduSpider()
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    # 允许所有CORS访问
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
 
 
-@app.get('/web')
-async def search_web(query: str, page: int = 1) -> dict:
+@app.route('/web')
+def search_web():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
     return {
         'status': 'success',
         'results': spider.search_web(query, page)
     }
 
 
-@app.get('/pic')
-async def search_pic(query: str, page: int = 1):
+@app.route('/pic')
+def search_pic():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
     return {
         'status': 'success',
         'results': spider.search_pic(query, page)
     }
 
 
-@app.get('/zhidao')
-async def search_zhidao(query: str, page: int = 1):
+@app.route('/zhidao')
+def search_zhidao():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
     return {
         'status': 'success',
         'results': spider.search_zhidao(query, page)
     }
 
 
-@app.get('/video')
-async def search_video(query: str, page: int = 1):
+@app.route('/video')
+def search_video():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
     return {
         'status': 'success',
         'results': spider.search_video(query, page)
     }
 
 
-@app.get('/news')
-async def search_news(query: str, page: int = 1):
+@app.route('/news')
+def search_news():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
     return {
         'status': 'success',
         'results': spider.search_news(query, page)
     }
 
 
-@app.get('/wenku')
-async def search_wenku(query: str, page: int = 1):
+@app.route('/wenku')
+def search_wenku():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
     return {
         'status': 'success',
         'results': spider.search_wenku(query, page)
     }
 
 
-@app.get('/jingyan')
-async def search_jingyan(query: str, page: int = 1):
+@app.route('/jingyan')
+def search_jingyan():
+    query = request.args.get('query')
+    page = request.args.get('page', 1)
     return {
         'status': 'success',
         'results': spider.search_jingyan(query, page)
     }
 
 
-@app.get('/baike')
-async def search_baike(query: str):
+@app.route('/baike')
+def search_baike():
+    query = request.args.get('query')
     return {
         'status': 'success',
         'results': spider.search_baike(query)
     }
 
 
-@app.get('/status')
-async def get_status():
+@app.route('/status')
+def get_status():
     return {
         'status': 'online'
     }
+
+
+if __name__ == '__main__':
+    app.run()
